e1de61dec9f520fe4cac4aabe761a10f3314df88
chooyen <chooyen@tp.edu.sg>
2019-07-22 21:23:29 +0800

    1st commit

---
+++ ggplotR.R
@@ -0,0 +1,10 @@
+##################################################
+## Project:source: http://r-statistics.co/ggplot2-Tutorial-With-R.html
+## Script purpose:Learn GG Plot from r-statistics
+## Date:
+## Author: 
+##################################################
+
+library(tidyverse)
+
+glimpse(diamonds
